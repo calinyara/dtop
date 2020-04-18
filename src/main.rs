c83bd88f87c84a04572c9cdee6068b4eeaca144b
Calinyara <mr.dengjie@gmail.com>
2020-04-18 16:50:43 +0800

    Use scores

--- src/main.rs
+++ src/main.rs
@@ -43,7 +43,7 @@ fn main() {
     }
 
     /*
-     * Start a thread on each physical core and do ticks statistics
+     * Start a thread on each physical core and do scores statistics
      * info: (CoreId, (Sender<i32>, Receiver<i32>))
      */
     let threads_info: Vec<_> = core_ids.into_iter().zip(channels.into_iter()).collect();
@@ -76,33 +76,33 @@ fn main() {
     tokio::run(task);
 
     /*
-     * System Ticks Statistics
+     * System Scores Statistics
      */
-    let mut ticks = vec![];
+    let mut scores = vec![];
     for handle in handles.into_iter() {
-        let tick = handle.join().unwrap();
-        ticks.push(tick);
+        let score = handle.join().unwrap();
+        scores.push(score);
     }
 
-    let total_ticks: i64 = ticks.iter().sum::<u32>() as i64;
+    let total_score: i64 = scores.iter().sum::<u32>() as i64;
     if matches.is_present("calibrate") {
         println!("Calibrating...");
-        println!("Ticks per CPU: {:?}", ticks);
-        println!("Total Ticks: {}", total_ticks);
-        save_calibration(total_ticks);
+        println!("Scores per CPU: {:?}", scores);
+        println!("Total Calibrated Score: {}", total_score);
+        save_calibration(total_score);
     } else {
-        match File::open("ticks.txt") {
+        match File::open("scores.txt") {
             Err(_) => {
                 println!("Calibrating... Run again to check the system load!");
-                println!("Ticks per CPU: {:?}", ticks);
-                println!("Total Ticks: {}", total_ticks);
-                save_calibration(total_ticks);
+                println!("Scores per CPU: {:?}", scores);
+                println!("Total Calibrated Score: {}", total_score);
+                save_calibration(total_score);
             },
             Ok(_) => {
-                let total_ticks: f64 = ticks.iter().sum::<u32>() as f64;
-                let calibration_ticks: f64 = get_calibration() as f64;
-                let sysload: f64 =  (calibration_ticks - total_ticks) /  calibration_ticks * 1000.;
-                println!("System Load {:.3}%", sysload);
+                let total_score: f64 = scores.iter().sum::<u32>() as f64;
+                let calibration_scores: f64 = get_calibration() as f64;
+                let sysload: f64 =  (calibration_scores - total_score) /  calibration_scores * 1000.;
+                println!("System Load {:.3}%, Total Score: {}", sysload, total_score);
             }
         }
     }
@@ -110,17 +110,17 @@ fn main() {
 
 fn calibrate(info: (CoreId, (Sender<i32>, Receiver<i32>))) -> u32 {
 
-    let mut tick: u32 = 0;
+    let mut score: u32 = 0;
 
     loop {
 
         for _ in 1..1_000 {}
 
-        tick += 1;
+        score += 1;
 
         match ((info.1).1).try_recv() {
             Ok(_) | Err(TryRecvError::Disconnected) => {
-                break tick;
+                break score;
             },
             Err(TryRecvError::Empty) => {},
         }
@@ -128,12 +128,12 @@ fn calibrate(info: (CoreId, (Sender<i32>, Receiver<i32>))) -> u32 {
 }
 
 fn save_calibration(t: i64) {
-    let mut file = File::create("ticks.txt").expect("create failed");
+    let mut file = File::create("scores.txt").expect("create failed");
     file.write_all(t.to_string().as_bytes()).expect("write failed");
 }
 
 fn get_calibration() -> i64 {
-    let mut file = File::open("ticks.txt").unwrap();
+    let mut file = File::open("scores.txt").unwrap();
     let mut s = String::new();
     file.read_to_string(&mut s).unwrap();
     s.parse::<i64>().unwrap()
